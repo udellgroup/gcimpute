9ce3d9c136c4259bd340d92387a7ba2a13bc5ae8
yuxuanzhao2295 <yz2295@cornell.edu>
2022-04-05 19:06:50 -0400

    robust helper

--- gcimpute/helper_mask.py
+++ gcimpute/helper_mask.py
@@ -23,22 +23,34 @@ def mask_types(X, mask_num, seed, var_types):
     return X_masked
     
 
-def mask_MCAR(X, mask_fraction, seed=1, max_try=50, allow_empty_row=False):
+def mask_MCAR(X, mask_fraction, seed=1, allow_empty_row=False, silence_rate=0.01):
     """
     Masks mask_fraction entries of X, raising a value error if an entire row is masked
     """
     X = np.asarray(X)
     rng = np.random.default_rng(seed)
-    count = 0
     X_masked = np.copy(X) 
     obs_coors = np.argwhere(~np.isnan(X))
     n_obs = obs_coors.shape[0]
-    p = X.shape[1]
+    n,p = X.shape
 
+    mask_indices = rng.choice(n_obs, size=int(mask_fraction*n_obs), replace=False)
+    mask_coors = obs_coors[mask_indices]
+    X_masked[mask_coors[:,0], mask_coors[:,1]] = np.nan
+    which_empty = np.flatnonzero((~np.isnan(X_masked)).sum(axis=1) == 0) 
+    if not allow_empty_row:
+        for row in which_empty:
+            obs_loc = np.flatnonzero(~np.isnan(X[row,:]))
+            index = rng.choice(obs_loc, size=1)
+            X_masked[row,index] = X[row,index]
+
+        r = (np.isnan(X_masked).sum() - (n*p-n_obs))/n_obs
+        if r<mask_fraction-silence_rate:
+            print(f'Actual masking ratio: {np.round(r, 4)}')
+    return X_masked
+    """
+    count = 0
     while True:
-        mask_indices = rng.choice(n_obs, size=int(mask_fraction*n_obs), replace=False)
-        mask_coors = obs_coors[mask_indices]
-        X_masked[mask_coors[:,0], mask_coors[:,1]] = np.nan
         # the masking is acceptable if there is no empty row
         if allow_empty_row or np.isnan(X_masked).sum(axis=1).max() < p:
             break
@@ -48,4 +60,21 @@ def mask_MCAR(X, mask_fraction, seed=1, max_try=50, allow_empty_row=False):
         if count == max_try:
             raise ValueError("Failure in masking data without empty rows")
     return X_masked
+    """
+
+def split_mask_val_test(X_mask, X, val_ratio = 0.5, rng = None, seed = 1):
+    if rng is None:
+        rng = np.random.default_rng(seed)
+    mask_coors = np.argwhere(~np.isnan(X) & np.isnan(X_mask))
+    n = len(mask_coors)
+    index_val = rng.choice(np.arange(n), size=int(n*val_ratio), replace=False)
+    index_test = np.setdiff1d(np.arange(n), index_val)
+    X_val = X_mask.copy()
+    X_test = X_mask.copy()
+    r = {'validation':X_mask.copy(), 'test':X_mask.copy()}
+    loc = {'validation':index_val, 'test':index_test}
+    for name,v in loc.items():
+        r[name][mask_coors[v,0], mask_coors[v,1]] = X[mask_coors[v,0], mask_coors[v,1]]
+    return r
+    
 
