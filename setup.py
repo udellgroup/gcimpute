9697cfbf5217189d6542b413cdaaef4da04854da
yuxuanzhao2295 <yz2295@cornell.edu>
2022-04-29 15:39:27 -0400

    mark version as 0.0.2 (release soon)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='gcimpute',
-    version='0.0.1',
+    version='0.0.2',
     description='A missing value imputation algorithm using the Gaussian copula model',
     long_description=long_description,
     long_description_content_type='text/markdown',
